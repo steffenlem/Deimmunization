23dffe8db0a667620bcd79de82b1c65656888d67
steffenlem <steffen_lemke@live.de>
2018-09-26 04:47:42 +0200

    Simple epitope visualization

--- Deimmunization.py
+++ Deimmunization.py
@@ -1,13 +1,17 @@
 import subprocess
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    result = subprocess.run(['/home/steffen/netMHCIIpan-3.2/netMHCIIpan', '-f', '/home/steffen/netMHCIIpan-3.2/test/example.fsa', '-a', 'DRB1_0101'], stdout=subprocess.PIPE)
+
+    ###Transfer to separate module###
+    #Argparser...
+    result = subprocess.run(['/home/steffen/netMHCIIpan-3.2/netMHCIIpan', '-f', 'data/BoTC_mutations_TEV.fasta', '-a', 'DRB1_0301'], stdout=subprocess.PIPE)
 
 
     a = str(result.stdout)
@@ -17,10 +21,12 @@ if __name__ == '__main__':
     output = [x.split() for x in a.split('\\n')]
     output = [x for x in output if x != []]
     parsed_data = []
+    seq_length = 15
     for x in output:
         try:
             #check wether the line is data starting with a int
             int(x[0])
+            seq_length += 1
             #if the peptide is a binder (IC50 < 500 nM), then it is included
             if(float(x[8]) < 500):
                 parsed_data.append([int(x[0]), x[1], x[2], int(x[4]) + int(x[0]), x[5], float(x[8])])
@@ -28,18 +34,21 @@ if __name__ == '__main__':
             #no prediction data -> do nothing
             pass
 
-
-
-    print(parsed_data)
     print(np.array(parsed_data))
 
+    ###Transfer to separate module###
+    #Simple Visualization of Epitopes and binding cores
+    number_epitopes = np.zeros(seq_length)
+    for x in parsed_data:
+        for z in range(9):
+            number_epitopes[int(x[3])-1+z] =  number_epitopes[int(x[3])-1+z] + 1
 
 
 
-
-
-
-
-
+    #Time
     print()
     print(time.time() - start_time, "seconds")
+
+    #Plot Binding Cores
+    plt.plot(number_epitopes)
+    plt.show()
