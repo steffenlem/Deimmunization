1f141459f98c3483a50d28fb6bec7e504cfab7af
steffenlem <steffen_lemke@live.de>
2018-10-08 08:54:34 +0200

    Addition of point mutation class

---
+++ src/PointMutation/PointMutation.py
@@ -0,0 +1,63 @@
+class PointMutation:
+    """
+    instance of the class store the information about the point mutation itself and its effect
+    """
+
+    def __init__(self, amino_acid, index):
+        """
+        Constructor
+        :param amino_acid: amino_acid after mutation
+        :param index: index of point mutation
+        """
+        self.amino_acid = amino_acid
+        self.index = index
+        self.immunogenicity = 0
+        self.ddG = 0
+
+    @property
+    def amino_acid(self):
+        return self._amino_acid
+
+    @amino_acid.setter
+    def amino_acid(self, value):
+        self._amino_acid = value
+
+    @amino_acid.deleter
+    def amino_acid(self):
+        del self.amino_acid
+
+    @property
+    def index(self):
+        return self._amino_acid
+
+    @index.setter
+    def index(self, value):
+        self._index = value
+
+    @index.deleter
+    def index(self):
+        del self.index
+
+    @property
+    def immunogenicity(self):
+        return self._immunogenicity
+
+    @immunogenicity.setter
+    def immunogenicity(self, value):
+        self._immunogenicity = value
+
+    @immunogenicity.deleter
+    def immunogenicity(self):
+        del self.immunogenicity
+
+    @property
+    def ddG(self):
+        return self._ddG
+
+    @ddG.setter
+    def ddG(self, value):
+        self._ddG = value
+
+    @ddG.deleter
+    def ddG(self):
+        del self.ddG
