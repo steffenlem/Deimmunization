a6b3d39bfcd06a85525e3dc81e8f86dafe485549
Zethson <lukas.heumos@gmail.com>
2018-10-12 12:08:55 +0200

    [REFACTORING]

--- src/evaluation/epitope_per_index_plot.py
+++ src/evaluation/epitope_per_index_plot.py
@@ -31,16 +31,16 @@ after = [[26, 16], [27, 16], [28, 16], [29, 16], [30, 16], [31, 16], [32, 16], [
          [134, 1],
          [135, 1], [136, 1], [137, 1], [138, 1], [139, 1]]
 
-width = 0.5  # gives histogram aspect to the bar diagram
-
 import matplotlib.pyplot as plt
 
 import numpy as np
 
+width = 0.5  # gives histogram aspect to the bar diagram
+
 
 def plot_epitope_spread(epitopes_per_index_before, epitopes_per_index_after, protein_length):
     """
-    plots the epitope spread as a barplot with an additional lineplot
+    plots the epitope spread as a barplot with an additional lineplot -> shows the plots
     :param epitopes_per_index_before: expected to be sorted by epitope count!
     :param epitopes_per_index_after: expected to be sorted by epitope count!
     """
@@ -66,12 +66,21 @@ def plot_epitope_spread(epitopes_per_index_before, epitopes_per_index_after, pro
 
 
 def keep_only_nth_label(ax):
-    # Keeps every nth label
+    """
+    hides all labels except the nth
+    :param ax:
+    :return:
+    """
     n = 10
     [l.set_visible(False) for (i, l) in enumerate(ax.xaxis.get_ticklabels()) if i % n != 0]
 
 
 def sort_epitope_pred_per_index(epitopes_per_index):
+    """
+    sorts epitopes per index lists per index
+    :param epitopes_per_index:
+    :return:
+    """
     epitopes_per_index_before_np = np.array(epitopes_per_index)
     sorted_epitopes_per_index = sorted(epitopes_per_index_before_np, key=lambda x: x[0])
     sorted_epitopes_np = np.array(sorted_epitopes_per_index)
@@ -79,9 +88,15 @@ def sort_epitope_pred_per_index(epitopes_per_index):
 
 
 def fill_missing_val_epitope_pred(epitopes_per_index, protein_length):
+    """
+    any missing values are added with an epitope count of 0
+    :param epitopes_per_index:
+    :param protein_length:
+    :return:
+    """
     sorted_epitope_per_index = sorted(epitopes_per_index, key=itemgetter(0))  # sort per index
 
-    # fill in missing values
+    # fill in missing values from index 1 to the currently already included amino acids
     i = 1
     prev_val = 1
     for val in sorted_epitope_per_index:
@@ -91,6 +106,7 @@ def fill_missing_val_epitope_pred(epitopes_per_index, protein_length):
         i = val[0] + 1
         prev_val = val[0] + 1
 
+    # add any additionally missing values from the end to the protein length
     if epitopes_per_index[len(epitopes_per_index) - 1][0] < protein_length:
         print(len(epitopes_per_index))
         for index in range(protein_length - len(epitopes_per_index)):
@@ -100,7 +116,14 @@ def fill_missing_val_epitope_pred(epitopes_per_index, protein_length):
 
 
 def create_epitope_pred_plot(X, y, plot_number, highest_epitope_count):
-    # plot before
+    """
+    creates a barplot including a lineplot
+    :param X:
+    :param y:
+    :param plot_number:
+    :param highest_epitope_count:
+    :return:
+    """
     pos = np.arange(len(y))
 
     ax = plt.axes()
